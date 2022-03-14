52f0bd8c034458098242ca49ff6d5afdc9b5e84b
Varad Gauthankar <gvarad1601@gmail.com>
2022-03-14 21:38:11 +0530

    showing journal created date

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -8,4 +8,9 @@ class DateFormatter {
   static String formatToAppStandardWithYear(String date) {
     return DateFormat('EEE, dd MMM yyy').format(DateTime.parse(date));
   }
+
+  static String getJournalCreatedDateWithTime(String date) {
+    return DateFormat('dd MMM yyy \'at\' hh:mm aa')
+        .format(DateTime.parse(date));
+  }
 }
