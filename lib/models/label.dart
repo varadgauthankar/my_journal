67115e649d8e49fc64ca07ae8fd6fae478632ab0
Varad Gauthankar <gvarad1601@gmail.com>
2022-07-01 23:52:32 +0530

    removed created at field from lagbegl

--- lib/models/label.dart
+++ lib/models/label.dart
@@ -3,26 +3,22 @@ import 'package:equatable/equatable.dart';
 class Label extends Equatable {
   String? label;
   String? id;
-  String? createdAt;
 
-  Label({this.id, this.label, this.createdAt});
+  Label({this.id, this.label});
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'label': label,
-        'createdAt': createdAt,
       };
 
   static Label fromJson(Map<String, dynamic> json) => Label(
         id: json['id'],
         label: json['label'],
-        createdAt: json['createdAt'],
       );
 
   Label.fromSnapshot(snapshot)
       : id = snapshot.data()['id'],
-        label = snapshot.data()['label'],
-        createdAt = snapshot.data()['createdAt'];
+        label = snapshot.data()['label'];
 
   @override
   List<Object?> get props => [label];
