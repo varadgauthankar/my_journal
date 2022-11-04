1662af97e29fb15fc46dc7ddb740c0b247908baa
Varad Gauthankar <gvarad1601@gmail.com>
2022-11-04 23:13:52 +0530

    changes made, able to run

---
+++ lib/pages/lock_screen_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LockScreenPage extends StatelessWidget {
+  const LockScreenPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Lock Screen'),
+      ),
+    );
+  }
+}
